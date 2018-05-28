7c0c068face29ac7494e4c633a796b5169248829
mathias <mathias.cabiochdelalande@gmail.com>
2018-05-28 13:48:38 +0200

    Mise a jour automatique

---
+++ Cpp/MainWindow.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_MAINWINDOW
+#define HEADER_MAINWINDOW
+
+#include <QtWidgets>
+
+class MainWindow : public QMainWindow {
+	Q_OBJECT
+	public:
+		MainWindow(QWidget* parent = NULL);
+		~MainWindow();
+	
+	public slots:
+		
+		
+	signals:
+		
+		
+	private:
+		
+};
+
+#endif //HEADER_MAINWINDOW
