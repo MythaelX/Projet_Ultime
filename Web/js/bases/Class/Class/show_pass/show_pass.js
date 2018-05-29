a2e2b8f80cf244dfffe4fdd0a6d385287d272449
mathias <mathias.cabiochdelalande@gmail.com>
2018-05-29 14:15:46 +0200

    Mise a jour automatique

--- Web/js/bases/Class/Class/show_pass/show_pass.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Create a show/hide effect for password inputs */
-	document.addEventListener('DOMContentLoaded', function() {
-		var eyes = document.querySelectorAll('.shower');
-		
-		
-	});
-/*************************************************/
