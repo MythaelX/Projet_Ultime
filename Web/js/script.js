19d05b9216f65df9c015e762ae789e580aa608d6
mathias <mathias.cabiochdelalande@gmail.com>
2018-06-11 12:04:24 +0200

    Mise a jour automatique

--- Web/js/script.js
+++ Web/js/script.js
@@ -3,7 +3,7 @@
 function posMain(){
 	var main = $("main");
 	if(isIn("index.php")){
-		if(main.style.display == "none"){
+		if($("#accueilPerso").style.display == "none"){
 			main.style.marginTop = "5%";
 		} else {
 			main.style.marginTop = "8%";
