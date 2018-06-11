b3847fb51935ff412d5a0adb7483dbd1f03e7b36
MythaelX <even.clement@live.fr>
2018-06-11 09:20:35 +0200

    mise a jour web

--- Web/php/verificationToken.php
+++ Web/php/verificationToken.php
@@ -1,5 +1,5 @@
 <?php
-/*require_once("bases/_functions.php");
+require_once("bases/_functions.php");
 require_once("bases/errors.php");
 require_once("bases/bdd.php");
 require_once("bddConnect.inc");
@@ -7,12 +7,12 @@ require_once("fonctions.php");
 
 $bdd = new Bdd("mysql", $BDD_HOST, $BDD_NAME, $BDD_USER, $BDD_PASS);
 if(!verifieToken($bdd)){
-  if($_SERVER['PHP_SELF']!="/Web/index.php" && $_SERVER['PHP_SELF']!="/Web/palmares.php" && $_SERVER['PHP_SELF']!="/Web/inscription.php" && $_SERVER['PHP_SELF']!="/Web/connexion.php"){
+  if(strpos($_SERVER['PHP_SELF'], "index.php")==false && strpos($_SERVER['PHP_SELF'],"palmares.php")==false && strpos($_SERVER['PHP_SELF'], "inscription.php")==false  && strpos($_SERVER['PHP_SELF'], "connexion.php")==false ){
     header('Location: ./index.php');
   }
 }else{
-  if($_SERVER['PHP_SELF']=="/Web/inscription.php" || $_SERVER['PHP_SELF']=="/Web/connexion.php"){
+  if(strpos($_SERVER['PHP_SELF'], "inscription.php") || strpos($_SERVER['PHP_SELF'], "connexion.php"){
     header('Location: ./index.php');
   }
-}*/
+}
 ?>
