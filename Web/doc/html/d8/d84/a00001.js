7ed5704f2a79cb619c1d91c67d3f9588cb46f745
mathias <mathias.cabiochdelalande@gmail.com>
2018-06-13 11:39:47 +0200

    Mise a jour automatique

--- Web/doc/html/d8/d84/a00001.js
+++ Web/doc/html/d8/d84/a00001.js
@@ -1,7 +1,10 @@
 var a00001 =
 [
-    [ "affichageAccueil", "d8/d84/a00001.php#ad7bb1a471a64bb1b432a6429de39da82", null ],
+    [ "affichageAccueil", "d8/d84/a00001.php#a20e111c0bc912042bdcb1790699311de", null ],
     [ "affichageParties", "d8/d84/a00001.php#adbff0145fcc69a075dff45ab975c2b2d", null ],
+    [ "ajaxRequest", "d8/d84/a00001.php#a2e3f7b2aca271c0d87c61a33b0030941", null ],
+    [ "ajaxRequest", "d8/d84/a00001.php#a72e65efac2b35e0b0c277d2239b2cfaa", null ],
     [ "preventDefault", "d8/d84/a00001.php#abf5219628a269a37395ec5371980db8d", null ],
+    [ "difficulte", "d8/d84/a00001.php#ad71ab45724394ceb19d97c8180ab0e89", null ],
     [ "location", "d8/d84/a00001.php#a9e7735e216b2770753e10ca177623f0f", null ]
 ];
--- Web/doc/html/de/d5e/a00007.js
+++ Web/doc/html/de/d5e/a00007.js
@@ -1,8 +1,10 @@
 var a00007 =
 [
-    [ "affichageThemes", "de/d5e/a00007.php#aee6c29e01ed8d036be6656155f1080fe", null ],
-    [ "ajaxRequest", "de/d5e/a00007.php#a806ccfa4bdd83d47638b934f63aa597f", null ],
-    [ "creerPartie", "de/d5e/a00007.php#ae2409b5e868763f39bddaca9e2c23035", null ],
-    [ "nbThemes", "de/d5e/a00007.php#abaac36f85ddb9ededb3d06ba67dca72e", null ],
-    [ "strict", "de/d5e/a00007.php#ae2475e10618961c050dcba04e8c42331", null ]
+    [ "ajaxRequest", "de/d5e/a00007.php#af2952b3a0807f32876447a30f44b4a4c", null ],
+    [ "preventDefault", "de/d5e/a00007.php#abf5219628a269a37395ec5371980db8d", null ],
+    [ "valideModification", "de/d5e/a00007.php#aa232ac3b0f1f501d720577dcf52fde2a", null ],
+    [ "false", "de/d5e/a00007.php#a5df37b7f02e5cdc7d9412b7f872b8e01", null ],
+    [ "location", "de/d5e/a00007.php#a9e7735e216b2770753e10ca177623f0f", null ],
+    [ "pseudo", "de/d5e/a00007.php#a1f0d86ab32405786812d806542ca3280", null ],
+    [ "verifBDDPseudo", "de/d5e/a00007.php#a558403c3a40c010539026c0e7e8a7b49", null ]
 ];
